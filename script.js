023963af75116d6e7959accb552733c26198c787
Lembont <creteurterence18@gmail.com>
2024-10-17 15:16:00 +0200

    [DONE] Finish front

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 addEventListener("DOMContentLoaded", () => {
-    // DIFFICULTY SELECTOR
+    // Le JS pour la section de séléction de la difficulté
     const facile = document.getElementById("facile");
     const moyen = document.getElementById("moyen");
     const difficile = document.getElementById("difficile");
@@ -8,18 +8,25 @@ addEventListener("DOMContentLoaded", () => {
     const inputNumber = document.getElementById("inputNumber");
     let DIFFICULTY = '';
 
+
     facile.addEventListener("click", () => {
-        explication.innerHTML = 'Le nombre mystère est compris entre 1 et 10 ! Vous disposez de 3 essai(s) !';
+        explication.innerHTML = 'Le nombre mystère est compris entre 1 et 10 ! Vous disposez de 3 essais !';
+        explication.style.color = '#28A745';
+        explication.style.textShadow = '0 0 5px #28A745';
         DIFFICULTY = 'facile';
     });
     
     moyen.addEventListener("click", () => {
-        explication.innerHTML = 'Le nombre mystère est compris entre 1 et 50 ! Vous disposez de 2 essai(s) !';
+        explication.innerHTML = 'Le nombre mystère est compris entre 1 et 50 ! Vous disposez de 2 essais !';
+        explication.style.color = '#FFC107';
+        explication.style.textShadow = '0 0 5px #FFC107';
         DIFFICULTY = 'moyen';
     });
     
     difficile.addEventListener("click", () => {
-        explication.innerHTML = 'Le nombre mystère est compris entre 1 et 100 ! Vous disposez de 1 essai(s) !';
+        explication.innerHTML = 'Le nombre mystère est compris entre 1 et 100 ! Vous disposez de 1 essai !';
+        explication.style.color = '#C82333';
+        explication.style.textShadow = '0 0 5px #C82333';
         DIFFICULTY = 'difficile';
     });
 
@@ -31,6 +38,8 @@ addEventListener("DOMContentLoaded", () => {
         explication.style.display = "none";
         document.getElementById("main").style.display = "block";
 
+        // Ici je passe ma fonction avec des paramètres qui définirons mes const et mes let
+
         if (DIFFICULTY === 'facile') {
             main(3, 10, 1, 10);
         }
@@ -40,15 +49,24 @@ addEventListener("DOMContentLoaded", () => {
         if (DIFFICULTY === 'difficile') {
             main(1, 100, 1, 100); 
         }
+        if (DIFFICULTY === '') {
+            main(3, 10, 1, 10);
+        }
     });
 
-    // MAIN
+
+
+    // Ma fonction main, le coeur du jeu
     function main(health, difficult_value, inputValuemin, inputValuemax) {
         const mystere = document.getElementById("mysterie-container");
         const validate = document.getElementById("validate");
         const mystereValue = Math.floor(Math.random() * difficult_value) + 1;
         const replay = document.getElementById("replay");
+        const body = document.querySelector("body");
         let count = 0;
+        shakeElement(validate, 2000);
+
+    
 
         inputNumber.min = inputValuemin;
         inputNumber.max = inputValuemax;
@@ -65,18 +83,34 @@ addEventListener("DOMContentLoaded", () => {
                 mystere.innerHTML = "Bravo ! Vous avez gagné ! Vous avez trouvé le nombre mystère en " + count + " tentative(s) !";
                 replay.style.display = "block";
                 validate.style.display = "none";
+                body.classList.remove("neutral", "lose");
+                body.classList.add("win");
+                ShootSomeStars();
+                
+                
             } else if (inputValue > mystereValue) {
-                mystere.innerHTML = "Vous avez écrit " + inputValue + "... Mais il est bien trop grand ! Plus que..." + (health - 1) + " essai(s) !";
+                mystere.innerHTML = "Vous avez écrit " + inputValue + "... Mais il est trop grand ! Plus que..." + (health - 1) + " essai(s) !";
+                validate.className = "btn btn-danger";
+                shakeElement(validate, 2000);
                 health--;
+                body.classList.remove("neutral", "win");
+                body.classList.add("lose");
             } else if (inputValue < mystereValue) {
-                mystere.innerHTML = "Vous avez écrit " + inputValue + "... Mais il est bien trop petit ! Plus que..." + (health - 1) + " essai(s) !";
+                mystere.innerHTML = "Vous avez écrit " + inputValue + "... Mais il est trop petit ! Plus que..." + (health - 1) + " essai(s) !";
+                validate.className = "btn btn-danger";
+                shakeElement(validate, 2000);
                 health--;
+                body.classList.remove("neutral", "win");
+                body.classList.add("lose");
             }
 
             if (health === 0) {
                 mystere.innerHTML = "Perdu ! Le nombre était " + mystereValue + " !";
                 replay.style.display = "block";
+                replay.className = "btn btn-danger border border-white";
                 validate.style.display = "none";
+                body.classList.remove("neutral", "win");
+                body.classList.add("lose");
             }
         });
 
@@ -85,3 +119,54 @@ addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+
+function ShootSomeStars() {
+
+    const defaults = {
+        spread: 360,
+        ticks: 50,
+        gravity: 0,
+        decay: 0.94,
+        startVelocity: 30,
+        shapes: ["star"],
+        colors: ["FFE400", "FFBD00", "E89400", "FFCA6C", "FDFFB8"],
+      };
+      
+
+// Fonction qui permet l'envoie de confetti après avoir gagné
+
+      function shoot() {
+        confetti({
+          ...defaults,
+          particleCount: 40,
+          scalar: 1.2,
+          shapes: ["star"],
+        });
+      
+        confetti({
+          ...defaults,
+          particleCount: 10,
+          scalar: 0.75,
+          shapes: ["circle"],
+        });
+      }
+      
+      setTimeout(shoot, 0);
+      setTimeout(shoot, 100);
+      setTimeout(shoot, 200);
+
+    }
+
+// Fonction qui permet de faire trembler le bouton
+
+    function shakeElement(element, duration) {
+        element.classList.remove("shake");
+        setTimeout(() => {
+            element.classList.add("shake");
+    
+
+            setTimeout(() => {
+                element.classList.remove("shake");
+            }, duration);
+        }, 10); 
+    }
